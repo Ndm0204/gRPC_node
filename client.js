1fd1e0b8a7e650365d8900eaf8ccfc1449c4f9e6
I344202 <nadeem.beg@sap.com>
2022-04-11 11:25:31 +0530

    client for test

--- client.js
+++ client.js
@@ -6,11 +6,33 @@ const DMSPackage = grpcObject.DMSPackage;
 
 const client = new DMSPackage.DMSService('localhost:40000', grpc.credentials.createInsecure());
 
-client.createFolder({
-    name:'folder3',
-    parent:'./',
-    owner:'54f0d030-b8cc-11ec-a40b-2940da2a6b83'},(err,data)=>{
-    if(err) throw err;
-    console.log(JSON.stringify(data));
+// client.createFolder({
+//     name:'folder',
+//     parent:'',
+//     owner:'54f0d030-b8cc-11ec-a40b-2940da2a6b83'},(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
     
-})
+// })
+
+// client.getAll({
+//     owner:'54f0d030-b8cc-11ec-a40b-2940da2a6b83'
+// },(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+client.createFile({
+    name:'file3',
+    content:'Testing this application 2',
+    parent:'',
+    owner:'54f0d030-b8cc-11ec-a40b-2940da2a6b83'
+    },(err,data)=>{
+        if(err) throw err;
+        console.log(data);}
+)
+// client.getFile({_id:'0c530cc0-b959-11ec-9b85-ed71eda4979a'},
+// (err,data)=>{
+//     if(err) console.log(err);
+//     console.log(data);
+// })
