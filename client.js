5210bb69d380e37bad92e290b28c2b550b1e3c82
I344202 <nadeem.beg@sap.com>
2022-04-11 12:03:36 +0530

    client

--- client.js
+++ client.js
@@ -22,17 +22,24 @@ const client = new DMSPackage.DMSService('localhost:40000', grpc.credentials.cre
 //     console.log(data);
 // })
 
-client.createFile({
-    name:'file3',
-    content:'Testing this application 2',
-    parent:'',
-    owner:'54f0d030-b8cc-11ec-a40b-2940da2a6b83'
-    },(err,data)=>{
-        if(err) throw err;
-        console.log(data);}
-)
-// client.getFile({_id:'0c530cc0-b959-11ec-9b85-ed71eda4979a'},
+// client.createFile({
+//     name:'file3',
+//     content:'Testing this application 2',
+//     parent:'',
+//     owner:'54f0d030-b8cc-11ec-a40b-2940da2a6b83'
+//     },(err,data)=>{
+//         if(err) throw err;
+//         console.log(data);}
+// )
+// client.getFiles({_id:'2cd6e9c0-b95a-11ec-87f4-6b3146739266'},
 // (err,data)=>{
 //     if(err) console.log(err);
 //     console.log(data);
 // })
+client.moveFile({
+    _id:'ab6b2110-b95b-11ec-92c9-b5abfc444e63',
+    parent:'2cd6e9c0-b95a-11ec-87f4-6b3146739266'
+},(err,data)=>{
+    if(err) console.log('error while moving file');
+    console.log(data);
+})
